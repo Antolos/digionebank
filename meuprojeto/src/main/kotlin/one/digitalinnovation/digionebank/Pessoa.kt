d1e243505f1a105fd3938dd53316e065fe5c434e
Antolos <acdnf@yahoo.com.br>
2021-07-19 17:59:39 -0300

    updated to make peson and employee abstract class

--- meuprojeto/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ meuprojeto/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,29 +1,39 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Jether"
-
-    var cpf: String = "123.456.789.12"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
+abstract class Pessoa (
+    val nome:String,
+    val cpf: String
+)
 
 
-    //inner  class Endereco{                                // Não será usado inner class
-    //    var rua: String = "Rua teste"
-    //}
-}
-
-fun main() {
-    val jether = Pessoa()
-
-
-    println(jether.pessoaInfo())
-
-
-    //jether.cpf = "4567" private set não permite alteração fora da classe porém permite o get
-    //println(jether.cpf)
-    //print(jether.Endereco().rua)// Não será usado inner class
-}
+// classe open exemplo
+//open class Pessoa (
+//    open val nome:String,
+//    open val cpf: String
+//)
+// limpando o codigo para versao final
+// var nome: String = "Jether"
+//
+// var cpf: String = "123.456.789.12"
+// private set
+//
+// constructor()
+//
+// fun pessoaInfo() = "$nome e $cpf"
+//
+//
+// //inner  class Endereco{                                // Não será usado inner class
+// //    var rua: String = "Rua teste"
+// //}
+// }
+//
+// fun main() {
+// val jether = Pessoa()
+//
+//
+// println(jether.pessoaInfo())
+//
+//
+// //jether.cpf = "4567" private set não permite alteração fora da classe porém permite o get
+// //println(jether.cpf)
+// //print(jether.Endereco().rua)// Não será usado inner class
